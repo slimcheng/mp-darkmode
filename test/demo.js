4615b9efc0f54bc11b48833c83ddeb9481fd37ac
JaminQ <jaminqian@outlook.com>
2020-04-16 21:03:58 +0800

    up

--- test/demo.js
+++
@@ -1,4 +0,0 @@
-var container = document.getElementById('dark');
-container.innerHTML = document.getElementById('light').innerHTML;
-// Darkmode.run(container.querySelectorAll('*'));
-container.style.visibility = 'visible';
